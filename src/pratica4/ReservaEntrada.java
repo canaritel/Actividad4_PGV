4b96bc4da54428183852f313e68d9e1e28594482
Antonio Gonzalez <toni@wificlick.es>
2021-10-29 19:16:47 +0100

    OpenBeans:Actividad Práctica 4 Asignatura PGV

---
+++ src/pratica4/ReservaEntrada.java
@@ -0,0 +1,18 @@
+package pratica4;
+//
+public class ReservaEntrada extends Thread {
+
+    private final Servidores servidor;
+    private final Usuario usuario;
+
+    public ReservaEntrada(Servidores servidor, Usuario usuario) {
+        this.servidor = servidor;
+        this.usuario = usuario;
+    }
+
+    @Override
+    public void run() {
+        servidor.reservar(usuario);
+    }
+
+}
